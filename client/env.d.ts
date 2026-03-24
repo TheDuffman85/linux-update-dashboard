a5ded9cc086012a82cf09ab09409a119bda717a9
TheDuffman85 <benjamin.tausend@googlemail.com>
2026-03-24 23:26:26 +0100

    chore: combine dependabot version bumps (#102)

--- client/env.d.ts
+++ client/env.d.ts
@@ -3,3 +3,5 @@ declare const __APP_BUILD_DATE__: string;
 declare const __APP_COMMIT_HASH__: string;
 declare const __APP_BRANCH__: string;
 declare const __APP_REPO_URL__: string;
+
+declare module "*.css";
